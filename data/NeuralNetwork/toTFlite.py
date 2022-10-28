538c92d09b9b1de77785c3375b315fa9999baa74
Marco Motamed <100919731+MotaMarco@users.noreply.github.com>
2022-10-28 12:57:02 +0200

    Tflite model

---
+++ data/NeuralNetwork/toTFlite.py
@@ -0,0 +1,12 @@
+##Used by GOOGLE COLAB
+import tensorflow as tf
+
+#Path to the neural network to convert to tflite
+model_dir="/content/NeuralNetwork.h5"
+
+model = tf.keras.models.load_model(model_dir)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+# Save the model
+with open('model.tflite',"wb") as f:
+  f.write(tflite_model)
